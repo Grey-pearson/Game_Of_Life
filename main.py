574ebd7cf6e04dc85666902ead16a6f21c85a175
grey <greytpearson@gmail.com>
2024-06-18 11:13:43 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,51 @@
 import sys
 import time
-from collections import defaultdict
+from collections import defaultdict, namedtuple
 from copy import deepcopy
 import pygame
 
-from setup import GOSPER_GLIDER, Grid, Neighbours
+Dim = namedtuple("Dimension", ["width", "height"])
+Grid = namedtuple("Grid", ["dim", "cells"])
+Neighbours = namedtuple("Neighbours", ["alive", "dead"])
+
+GOSPER_GLIDER = Grid(
+    Dim(50, 50),
+    {
+        (22, 8),
+        (12, 7),
+        (36, 7),
+        (17, 9),
+        (11, 8),
+        (1, 9),
+        (25, 4),
+        (2, 8),
+        (16, 7),
+        (25, 10),
+        (21, 6),
+        (23, 9),
+        (14, 6),
+        (36, 6),
+        (22, 7),
+        (14, 12),
+        (17, 8),
+        (11, 10),
+        (25, 9),
+        (35, 7),
+        (1, 8),
+        (18, 9),
+        (22, 6),
+        (21, 8),
+        (23, 5),
+        (12, 11),
+        (17, 10),
+        (11, 9),
+        (35, 6),
+        (25, 5),
+        (2, 9),
+        (13, 6),
+        (13, 12),
+        (15, 9),
+        (16, 11),
+        (21, 7),
+    },
+)
